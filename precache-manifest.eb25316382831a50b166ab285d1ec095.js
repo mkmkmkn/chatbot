2820ff5b1f8e7c8a0023543fbdbcb24e260c367f
unknown <mkgoma3@gmail.com>
2020-08-24 12:55:47 +0900

    Updates

---
+++ precache-manifest.eb25316382831a50b166ab285d1ec095.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbca4953f697b3a9f9a94c6768fa28ab",
+    "url": "/chatbot/index.html"
+  },
+  {
+    "revision": "ee1df31dc7071a75a8e4",
+    "url": "/chatbot/static/css/main.65b932f0.chunk.css"
+  },
+  {
+    "revision": "cf914fb4b101982b185c",
+    "url": "/chatbot/static/js/2.c8c38056.chunk.js"
+  },
+  {
+    "revision": "fbfb5e42a41e7004782949e93fdf296a",
+    "url": "/chatbot/static/js/2.c8c38056.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee1df31dc7071a75a8e4",
+    "url": "/chatbot/static/js/main.c25ec969.chunk.js"
+  },
+  {
+    "revision": "4b6b4c93f134e5e03cdb",
+    "url": "/chatbot/static/js/runtime-main.3c8cc0ae.js"
+  },
+  {
+    "revision": "5fecd33e45b07408d2594c28e848b949",
+    "url": "/chatbot/static/media/mk.5fecd33e.jpg"
+  },
+  {
+    "revision": "b17f9bb1269ce534c66c34946ba4f60a",
+    "url": "/chatbot/static/media/questioner.b17f9bb1.jpg"
+  }
+]);
